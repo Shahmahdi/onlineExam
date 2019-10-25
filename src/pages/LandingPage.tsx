ffae964e11234c9adb85b8fc6d38438ce96285ab
mahdi <shahmahdi11@gmail.com>
2019-10-26 01:34:59 +0600

    landing page partially done

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavbarTop } from '../components/common/Navbar'
-import { Button } from '@blueprintjs/core'
+import { Button, Card, Elevation } from '@blueprintjs/core'
 // import { Sidebar } from '../components/common/Sidebar'
 
 export const LandingPage = () => {
@@ -42,29 +42,55 @@ export const LandingPage = () => {
             </div>
           </div>
         </div>
-        <div className="vw-100 vh-100" style={{ backgroundColor: 'yellow' }}>
+        <div id="" className="vw-100 vh-100" style={{ backgroundColor: 'yellow' }}>
           <div className="flex h-100 items-center justify-center">
             <div className="flex items-center w-70">
               <div className="fl w-40 tc f1">
                 <p className="mb0 fw1">About</p>
                 <p className="mb0 b ">US</p>
               </div>
-              <div className="fl w-60 f3">
-              Simply put, without the jargons, Magnito
-              Digital is the largest digital agency of
-              Bangladesh with a team of more than 70
-              people. We are famous for our unconventional
-              company culture. Our experience and massive setup
-              allows us to deliver services at unparalleled
-              scale. We have built leading brands in the digital
-              space and are the go-to-agency for managing y
-              our digital products & services. Being one of
-              the most awarded digital agencies of Bangladesh,
-              Magnito Digital won Gold to become the ‘Campaign
-              Asia-Pacific : Digital Agency of the Year 2015’ in the ‘
-              Rest of South Asia’ category.
+              <div className="fl w-60 f4">
+                Simply put, without the jargons, Magnito
+                Digital is the largest digital agency of
+                Bangladesh with a team of more than 70
+                people. We are famous for our unconventional
+                company culture. Our experience and massive setup
+                allows us to deliver services at unparalleled
+                scale. We have built leading brands in the digital
+                space and are the go-to-agency for managing y
+                our digital products & services. Being one of
+                the most awarded digital agencies of Bangladesh,
+                Magnito Digital won Gold to become the ‘Campaign
+                Asia-Pacific : Digital Agency of the Year 2015’ in the ‘
+                Rest of South Asia’ category.
               </div>
             </div>
+          </div>
+        </div>
+        <div className="vw-100 vh-100">
+          <div className="flex h-100 items-center justify-center">
+            <div className="flex items-center w-80" style={{ height: '65%' }}>
+              <div className="f1 fl flex flex-column h-100 justify-center tc w-25">
+                <p className="mb0 fw1">OUR</p>
+                <p className="mb0 b ">COURSES</p>
+              </div>
+              <div className="fl flex h-100 w-75">
+                <Card className="fl w-third" interactive={true} elevation={Elevation.TWO}>
+                  <p className="f1 flex h-100 items-center justify-center">Bangla</p>
+                </Card>
+                <Card className="fl w-third" interactive={true} elevation={Elevation.TWO}>
+                  <p className="f1 flex h-100 items-center justify-center">English</p>
+                </Card>
+                <Card className="fl w-third" interactive={true} elevation={Elevation.TWO}>
+                  <p className="f1 flex h-100 items-center justify-center">Biology</p>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h3" style={{ backgroundColor: '#40c5f4' }}>
+          <div className="f4 flex h-100 items-center justify-center white">
+            2019 Redx Solutions. All Rights Reserved.
           </div>
         </div>
       </div>
